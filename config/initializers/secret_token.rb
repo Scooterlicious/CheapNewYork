acbd723707bde9a802aa24f5cfb519b4d3732182
Scooterlicious <ScottDavidFell@gmail.com>
2014-01-13 14:06:00 -0500

    Initial empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheapNewYork::Application.config.secret_token = '736771b18d2ecf262b08e91d46c78c082bb3bf9b40939b7462ee447e648bd5269f2eec62624fa4a5dc609e6ee71b29cb715f8ceb2806e8e7952fc781aaa5f989'
