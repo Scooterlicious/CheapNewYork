35f1c2ce1bcfe8260d36cbeb988b00e0487a51f6
Scooterlicious <ScottDavidFell@gmail.com>
2014-01-14 08:55:06 -0500

    adding sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,38 @@
 class SitesController < ApplicationController
+	def index
+		@sites = Site.all
+	end
+
+	def new
+		@band = Site.new()
+	end
+
+	# def create
+	# 	Site.create({
+	# 		name: params[:band][:name]
+	# 	})
+	# 	redirect_to sites_path
+	# end
+
+
+	def show
+		@site = Site.find(params[:id])
+	end	
+
+	# def destroy 
+	# 	Site.find(params[:id]).destroy
+	# 	redirect_to sites_path
+	# end	
+
+	# def edit
+	# 	@band = Site.find(params[:id])
+	# end
+
+	# def update
+	# 	@band = Site.find(params[:id])
+	# 	@band.name = params[:band][:name]
+	# 	@band.save
+	# 	redirect_to band_path(params[:id])
+	# end
+
 end
